cb46f47d9d25e257d7862e8fb1a87ad2f2f0564d
mikey6002 <tun32913@temple.edu>
2025-03-31 22:02:30 -0400

    days 8 and 9

---
+++ Day9/index.js
@@ -0,0 +1,24 @@
+console.log("Hello World")
+let nickname = "Akkon"
+let age = 21
+console.log(nickname,age);
+
+var firstN = "jake"
+const lastname = "Dog"
+console.log(firstN,lastname);
+let attendance = 70
+if (attendance > 80) {
+    console.log("you are ok")
+}else{
+    console.log("you are not ok")
+}
+let outside = "outside"
+
+console.log(outside)
+{
+    let outside = "outside in here"
+    console.log(outside)
+
+
+
+}
