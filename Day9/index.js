b13f661026d3d96437a93d2cb71447038542de4e
mikey6002 <tun32913@temple.edu>
2025-04-02 19:19:03 -0400

    day 10 things

--- Day9/index.js
+++ Day9/index.js
@@ -1,24 +1,19 @@
-console.log("Hello World")
-let nickname = "Akkon"
-let age = 21
-console.log(nickname,age);
-
-var firstN = "jake"
-const lastname = "Dog"
-console.log(firstN,lastname);
-let attendance = 70
-if (attendance > 80) {
-    console.log("you are ok")
-}else{
-    console.log("you are not ok")
+for(let char of "fruits"){
+    console.log(char);
 }
-let outside = "outside"
-
-console.log(outside)
-{
-    let outside = "outside in here"
-    console.log(outside)
 
 
 
+//functions
+function square(num){
+    return num * num
 }
+
+console.log(square(5))
+
+function divMod(x,y){
+    return [x/y,x%y]
+}
+
+const cookies = 17;
+const people =5;
