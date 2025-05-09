028a78e2cb503315adf97c3971152ca6aed18d58
mikey6002 <tun32913@temple.edu>
2025-05-09 19:26:01 -0400

    Day25

--- Day24/Techwise_Vite_Project/src/Components/UserCard.jsx
+++ Day24/Techwise_Vite_Project/src/Components/UserCard.jsx
@@ -1,27 +1,43 @@
-function string
+import { Card, CardContent, Typography } from "@mui/material";
+import { Avatar } from "@mui/material";
 
+function stringToColor(string) {
+    let hash = 0;
+    let i;
+  
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+  
+    let color = '#';
+  
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+  
+    return color;
+  }
 
-
-
-export default function UserCard() {
-    const user = {
-        username: "John Doe",
-        email: "joen@gmail.com",
-        phone: "1234567890",
-        balance: 1000,
+function stringAvatar(username) {
+    console.log(username)
+    return {
+      sx: {
+        bgcolor: stringToColor(username),
+      },
+      children: `${username.split(' ')[0][0]}${username.split(' ')[1][0]}`,
     };
+  }
 
-    return ( 
+export default function UserCard({username, email, balance}) {
+    return (
         <Card>
-
-
+            <CardContent>
+                <Avatar {...stringAvatar(username)} />
+                <Typography variant="h5">{username}</Typography>
+                <Typography variant="h6">{email}</Typography>
+                <Typography variant="h6">{balance}</Typography>
+            </CardContent>
         </Card>
-        <div className="user-card">
-            <h1>Name: {user.username}</h1>
-            <h2>Email: {user.email}</h2>
-            <h2>Phone: {user.phone}</h2>
-            <img src="src/assets/Vamp.jpg" alt="User Avatar" />
-            <p>Hello I am new here.</p>
-        </div>
-    );
+    )
 }
