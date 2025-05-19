b2b983d545d3fedbd8687286c7528af0e69cd4d6
mikey6002 <tun32913@temple.edu>
2025-05-18 22:41:07 -0400

    reorganize to have day 11

--- Day11/login.js
+++ Day11/login.js
@@ -1,30 +1,51 @@
-const loginForm = document.getElementById("loginForm")
-console.log(loginForm);
+const emailInput = document.querySelector('#email');
+const emailError = document.querySelector('#emailError');
+const passwordInput = document.querySelector('#password');
+const togglePassword = document.querySelector('#togglePassword');
+
+console.log(emailInput);
+console.log(emailError);
+console.log(passwordInput);
+console.log(togglePassword);
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 
-const emailInput = document.getElementById("email")
-const emailError = document.getElementById("emailError")
-const passWordInput = document.getElementById("password")
-const togglePassword = document.getElementById("password")
-
-const emailRegex = "^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$" 
-
-console.log (emailInput)
-console.log (passWordInput)
-console.log (togglePassword)
-console.log (emailError)
-
-
-emailInput.addEventListener("input",()=>{
+emailInput.addEventListener('focusout', (e) => {
+    // something@something.something
+    // e.preventDefault();
     console.log(emailInput.value);
-    if (!emailRegex.test(emailInput.value)){
-        emailError.classList.remove('hidden')
-    }else{
-        emailError.classList.add('hidden')
+    if (emailInput.value === '') {
+        emailError.classList.add('hidden');
+    } else if (!emailRegex.test(emailInput.value)) {
+        emailError.classList.remove('hidden');
+    } else {
+        emailError.classList.add('hidden');
     }
 })
 
-togglePassword.addEventListener('click', ()=>){
-    passWordInput.setAttribute
-}
+togglePassword.addEventListener('click', () => {
+    
+    const type = passwordInput.getAttribute('type');
+    console.log(type);
+    const newType = type === 'password' ? 'text' : 'password';
+    console.log(newType);
+    passwordInput.setAttribute('type', newType);
 
+})
+
+
+const loginForm = document.querySelector('#loginForm');
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e);
+
+    const emailValue = emailInput.value;
+    const passwordValue = passwordInput.value;
+
+    console.log(emailValue);
+    console.log(passwordValue);
+    
+    
+})
